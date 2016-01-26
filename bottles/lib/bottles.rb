767c9995e9c80b56206cdeffeb178957e056963f
Sandi Metz <sandi@sandimetz.com>
2016-01-26 14:29:45 -0600

    dry bottles updated

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -9,10 +9,18 @@ class Bottles
   end
 
   def verse(number)
-    "#{amount(number).capitalize} #{container(number)} of beer on the wall, " +
-    "#{amount(number)} #{container(number)} of beer.\n" +
+    "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
+    "#{quantity(number)} #{container(number)} of beer.\n" +
     "#{action(number)}, " +
-    "#{amount(successor(number))} #{container(successor(number))} of beer on the wall.\n"
+    "#{quantity(successor(number))} #{container(successor(number))} of beer on the wall.\n"
+  end
+
+  def quantity(number)
+    if number == 0
+      "no more"
+    else
+      number.to_s
+    end
   end
 
   def container(number)
@@ -23,27 +31,19 @@ class Bottles
     end
   end
 
-  def pronoun(number)
-    if number == 1
-      "it"
-    else
-      "one"
-    end
-  end
-
-  def amount(number)
-    if number == 0
-      "no more"
-    else
-      number.to_s
-    end
-  end
-
   def action(number)
     if number == 0
       "Go to the store and buy some more"
     else
       "Take #{pronoun(number)} down and pass it around"
+    end
+  end
+
+  def pronoun(number)
+    if number == 1
+      "it"
+    else
+      "one"
     end
   end
 
